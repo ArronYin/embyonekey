701c8e05a74f8b23ba28913d93d11c790e2ea667
s1oz <32033912+s1oz@users.noreply.github.com>
2020-04-28 14:18:24 +0800

    Update embyonekey.sh

--- embyonekey.sh
+++ embyonekey.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
-cd /etc/nginx/sites-enabled
-wget https://raw.githubusercontent.com/s1oz/embyonekey/master/proxy.conf
-curl https://raw.githubusercontent.com/s1oz/embyonekey/master/guomi.cer >> /etc/ssl/certs/ca-certificates.crt
 cd /var/packages/EmbyServer/target/mono/bin
+curl https://raw.githubusercontent.com/s1oz/embyonekey/master/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
 ./cert-sync /etc/ssl/certs/ca-certificates.crt
-cd /volume1/web/mb3admin.com
-wget https://raw.githubusercontent.com/s1oz/embyonekey/master/mb3admin.com.cert.pem
-wget https://raw.githubusercontent.com/s1oz/embyonekey/master/mb3admin.com.key.pem
-cat mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
 cd /usr/local/etc/nginx/conf.d/*-*-*-*
 wget https://raw.githubusercontent.com/s1oz/embyonekey/master/user.conf
 nginx -s reload
 done
+
